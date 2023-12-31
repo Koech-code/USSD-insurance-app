63596258beaa6a83cdd9f9b0c2de8a590d8d9923
Koech-code <nixonkipkorir01@gmail.com>
2023-12-31 12:25:20 +0300

    removed MongoDB database configuration and used MySQL

--- server.js
+++ server.js
@@ -1,60 +1,20 @@
-// // const express = require("express");
-// // const app = express();
-// // const port = 3000;
-
-// // const http = require("http");
-// // const httpServer = http.createServer(app);
-
-// // require("dotenv").config();
-// // const PORT = process.env.PORT;
-// // // Define a route
-// // app.get("/", (req, res) => {
-// //   res.send("API is running!");
-// // });
-
-// // var ussdRouter = require("./routes/ussdRoutes");
-// // app.use(ussdRouter);
-// // // Start the server
-// // httpServer.listen(PORT, () => {
-// //   console.log(`HTTP server is listening on port ${PORT}`);
-// // });
-
-// // module.exports = app;
-
-// const express = require("express");
-// const app = express();
-// const port = 3000;
-
-// const http = require("http");
-// const httpServer = http.createServer(app);
-
-// require("dotenv").config();
-// const PORT = process.env.PORT;
-
-// // Middleware to parse URL-encoded form data
-// app.use(express.urlencoded({ extended: true }));
-
-// // Middleware to parse JSON data
-// app.use(express.json());
-
-// // Define a route
-// app.get("/", (req, res) => {
-//   res.send("API is running!");
-// });
-
-// var ussdRouter = require("./routes/ussdRoutes");
-// app.use(ussdRouter);
-
-// // Start the server
-// httpServer.listen(PORT, () => {
-//   console.log(`HTTP server is listening on port ${PORT}`);
-// });
-
-// module.exports = app;
-
-// server.js
 const express = require("express");
 const bodyParser = require("body-parser");
+require("dotenv").config();
+// const mongoose = require("mongoose");
+
+// // mongoose database connection
+// async function connectToDatabase() {
+//   try {
+//     await mongoose.connect(process.env.DB_CONNECTION);
+//     console.log("Mongo DB connected");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//   }
+// }
+
+// connectToDatabase();
+
 const arkeselUssdRoutes = require("./routes/ussdRoute");
 
 const app = express();
