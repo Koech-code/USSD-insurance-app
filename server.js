fd91b247badc08c34deb6e901b8c8ed2c9f7254b
Koech-code <nixonkipkorir01@gmail.com>
2024-03-07 20:58:52 +0300

    created the https server

--- server.js
+++ server.js
@@ -1,27 +1,46 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const path = require("path");
+// const cors = require("cors");
+
+// require("dotenv").config();
+
+// const arkeselUssdRoutes = require("./routes/ussdRoute");
+
+// const app = express();
+// const port = 5000;
+// // Enable CORS for all routes
+// app.use(cors());
+
+// app.use(bodyParser.json());
+
+// // Mount the Arkesel USSD routes
+// app.use("/", arkeselUssdRoutes);
+
+// // Root route to serve the HTML file
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
+
+// // Start the Express server
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 
 require("dotenv").config();
-// const mongoose = require("mongoose");
-
-// // mongoose database connection
-// async function connectToDatabase() {
-//   try {
-//     await mongoose.connect(process.env.DB_CONNECTION);
-//     console.log("Mongo DB connected");
-//   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error);
-//   }
-// }
-
-// connectToDatabase();
 
 const arkeselUssdRoutes = require("./routes/ussdRoute");
 
 const app = express();
 const port = 5000;
+
 // Enable CORS for all routes
 app.use(cors());
 
@@ -35,7 +54,13 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Start the Express server
-app.listen(port, () => {
+// HTTPS options
+const httpsOptions = {
+  key: fs.readFileSync("./ssl/server.key"),
+  cert: fs.readFileSync("./ssl/server.crt"),
+};
+
+// Start the HTTPS server
+https.createServer(httpsOptions, app).listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
