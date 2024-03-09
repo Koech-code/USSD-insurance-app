72f3806f4310f4cc9458974045cb95f04e84c6e8
Koech-code <nixonkipkorir01@gmail.com>
2024-03-09 17:08:07 +0300

    changed the server to use https

--- server.js
+++ server.js
@@ -1,38 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const cors = require("cors");
-
-require("dotenv").config();
-
-const arkeselUssdRoutes = require("./routes/ussdRoute");
-
-const app = express();
-const port = 5000;
-// Enable CORS for all routes
-app.use(cors());
-
-app.use(bodyParser.json());
-
-// Mount the Arkesel USSD routes
-app.use("/", arkeselUssdRoutes);
-
-// Root route to serve the HTML file
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// Start the Express server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
 // const express = require("express");
 // const bodyParser = require("body-parser");
 // const path = require("path");
 // const cors = require("cors");
-// const https = require("https");
-// const fs = require("fs");
 
 // require("dotenv").config();
 
@@ -40,7 +9,6 @@ app.listen(port, () => {
 
 // const app = express();
 // const port = 5000;
-
 // // Enable CORS for all routes
 // app.use(cors());
 
@@ -54,13 +22,45 @@ app.listen(port, () => {
 //   res.sendFile(path.join(__dirname, "index.html"));
 // });
 
-// // HTTPS options
-// const httpsOptions = {
-//   key: fs.readFileSync("./ssl/server.key"),
-//   cert: fs.readFileSync("./ssl/server.crt"),
-// };
-
-// // Start the HTTPS server
-// https.createServer(httpsOptions, app).listen(port, () => {
+// // Start the Express server
+// app.listen(port, () => {
 //   console.log(`Server is running on port ${port}`);
 // });
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
+
+require("dotenv").config();
+
+const arkeselUssdRoutes = require("./routes/ussdRoute");
+
+const app = express();
+const port = 5000;
+
+// Enable CORS for all routes
+app.use(cors());
+
+app.use(bodyParser.json());
+
+// Mount the Arkesel USSD routes
+app.use("/", arkeselUssdRoutes);
+
+// Root route to serve the HTML file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// HTTPS options
+const httpsOptions = {
+  key: fs.readFileSync("./ssl/server.key"),
+  cert: fs.readFileSync("./ssl/server.crt"),
+};
+
+// Start the HTTPS server
+https.createServer(httpsOptions, app).listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
