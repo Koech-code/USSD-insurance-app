efdb4b14f55e4427a2395f7bf739f4fc944d5efe
Koech-code <nixonkipkorir01@gmail.com>
2024-03-10 08:18:32 +0300

    added send sms for successful transactions and saved a record of it in the db

--- routes/ussdRoute.js
+++ routes/ussdRoute.js
@@ -161,9 +161,9 @@ async function pay(amount, customerNumber, item_name, item_desc) {
       secrete: secret,
       isussd: true,
       newVodaPayment: true,
-      // carnums: carNum,
-      // whatsappNums: whatsappNum,
-      // item_name: item_name,
+      carnums: carNum,
+      whatsappNums: whatsappNum,
+      item_name: item_name,
       // username: process.env.NALO_USERNAME,
       // password: process.env.PASSWORD,
     },
@@ -186,7 +186,7 @@ async function pay(amount, customerNumber, item_name, item_desc) {
 
     console.log("Payment Params - ", response.data);
     InvoiceNo = response.data.InvoiceNo;
-    OrderId = response.data.order_id;
+    OrderId = response.data.Order_id;
     NumToSendSMS = customerNumber;
     // // Extract callback URL and customer number
     // const callbackUrl = response.data.callback;
@@ -5865,44 +5865,45 @@ router.post("/callback", async (req, res) => {
   res.status(200).json({ message: "callback success" });
   console.log("Invoive Number", InvoiceNo);
   console.log("Order ID", OrderId);
-  // if (
-  //   req.body.InvoiceNo === InvoiceNo &&
-  //   req.body.order_id === OrderId &&
-  //   req.body.status === "PAID"
-  // ) {
-  //   await PaymentResponse.create({
-  //     itemName: item_name,
-  //     amount: amount,
-  //     carnums: carNum,
-  //     phoneNumber: customerNumber,
-  //     whatsappnums: whatsappNum,
-  //     // Adding status field with a specific value
-  //     status: "pending", // Or you can omit this line to use the default value
-  //   });
+  console.log("Send SMS TO", NumToSendSMS);
+  if (
+    req.body.InvoiceNo === InvoiceNo &&
+    req.body.order_id === OrderId &&
+    req.body.status === "PAID"
+  ) {
+    await PaymentResponse.create({
+      itemName: item_name,
+      amount: amount,
+      carnums: carNum,
+      phoneNumber: customerNumber,
+      whatsappnums: whatsappNum,
+      // Adding status field with a specific value
+      status: "pending", // Or you can omit this line to use the default value
+    });
 
-  //   // send confirmation message
-  //   // Compose SMS message
-  //   const SMS_MESSAGE = `Thank you for choosing AEGIS RISK MANAGEMENT BROKERS. Your purchase is confirmed. Visit option 4, send required docs to WhatsApp +233591539372 or call us.`;
+    // send confirmation message
+    // Compose SMS message
+    const SMS_MESSAGE = `Thank you for choosing AEGIS RISK MANAGEMENT BROKERS. Your purchase is confirmed. Visit option 4, send required docs to WhatsApp +233591539372 or call us.`;
 
-  //   // Compose URL for sending SMS
-  //   const SEND_SMS_URL = `https://sms.arkesel.com/sms/api?action=send-sms&api_key=${process.env.ARKESEL_API_KEY}&to=${NumToSendSMS}&from=Flexible&sms=${SMS_MESSAGE}`;
+    // Compose URL for sending SMS
+    const SEND_SMS_URL = `https://sms.arkesel.com/sms/api?action=send-sms&api_key=${process.env.ARKESEL_API_KEY}&to=${NumToSendSMS}&from=Flexible&sms=${SMS_MESSAGE}`;
 
-  //   // Send SMS
-  //   const smsResponse = await axios.get(SEND_SMS_URL);
+    // Send SMS
+    const smsResponse = await axios.get(SEND_SMS_URL);
 
-  //   console.log("SMS Sent:", smsResponse.data.message);
-  // } else if (req.body.status === "FAILED") {
-  //   // Compose SMS message
-  //   const SMS_MESSAGE = `Hi, we noticed that your transaction failed. Please ensure that you have enough balance and get back or call us.`;
+    console.log("SMS Sent:", smsResponse.data.message);
+  } else if (req.body.status === "FAILED") {
+    // Compose SMS message
+    const SMS_MESSAGE = `Hi, we noticed that your transaction failed. Please ensure that you have enough balance and get back or call us.`;
 
-  //   // Compose URL for sending SMS
-  //   const SEND_SMS_URL = `https://sms.arkesel.com/sms/api?action=send-sms&api_key=${process.env.ARKESEL_API_KEY}&to=${NumToSendSMS}&from=Flexible&sms=${SMS_MESSAGE}`;
+    // Compose URL for sending SMS
+    const SEND_SMS_URL = `https://sms.arkesel.com/sms/api?action=send-sms&api_key=${process.env.ARKESEL_API_KEY}&to=${NumToSendSMS}&from=Flexible&sms=${SMS_MESSAGE}`;
 
-  //   // Send SMS
-  //   const smsResponse = await axios.get(SEND_SMS_URL);
+    // Send SMS
+    const smsResponse = await axios.get(SEND_SMS_URL);
 
-  //   console.log("SMS Sent:", smsResponse.data.message);
-  // }
+    console.log("SMS Sent:", smsResponse.data.message);
+  }
 });
 
 // Nalo solutions redirect  URL
