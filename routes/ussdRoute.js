0c67f7b9d1bf79907b46f5e08376f7e744a5fb17
Koech-code <nixonkipkorir01@gmail.com>
2024-05-31 13:25:40 +0300

    removed token

--- routes/ussdRoute.js
+++ routes/ussdRoute.js
@@ -164,8 +164,7 @@ async function pay(amount, customerNumber, item_name, item_desc) {
     url: `https://rmp.hubtel.com/merchantaccount/merchants/${process.env.HUBTEL_POS_SALES_ID}/receive/mobilemoney`,
     headers: {
       "Content-Type": "application/json",
-      "Authorization": "Basic MGxuQWx5OjQ4NzhjYjFlOTQ0YzQxMTc4NDBmZjMxNzM3MzY5Mzky"
-      // "Authorization": `Basic ${process.env.AUTHORIZATION_KEY}`
+      "Authorization": `Basic ${process.env.AUTHORIZATION_KEY}`
     },
     data: {
       // amount: amount,
